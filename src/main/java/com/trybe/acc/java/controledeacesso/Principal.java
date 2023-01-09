283df773288e2a3f4209b2184c7bd265a1b6ce2c
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-01-09 16:29:06 -0300

    Feat: add report

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,6 +1,9 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal {
@@ -11,6 +14,9 @@ public class Principal {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     ArrayList<Short> allCustomers = new ArrayList<Short>();
+    
+    int numberOption;
+    String age;
     
     do {
       System.out.println("Entre com o número correspondente à opção desejada:");
@@ -25,7 +31,7 @@ public class Principal {
       
       if (numberOption == 1) {
         System.out.println("Entre com a idade:");
-        int age = scanner.next();
+        age = scanner.next();
         Short ageNumber = Short.parseShort(age);
         
         allCustomers.add(ageNumber);
@@ -40,5 +46,48 @@ public class Principal {
       }
     } while (numberOption != 2);
     scanner.close();
+    
+    System.out.println("----- Quantidade -----");
+    int underAges = 0;
+    int adults = 0;
+    int overFifty = 0;
+    for (int index = 0; index < allCustomers.size(); index += 1) {
+      if (allCustomers.get(index) < 18) {
+        underAges += 1;
+      } else if (allCustomers.get(index) >= 18 && allCustomers.get(index) < 50) {
+        adults += 1;
+      } else {
+        overFifty += 1;
+      }
+    }
+    
+    System.out.println("menores: " + underAges);
+    System.out.println("adultas: " + adults);
+    System.out.println("a partir de 50: " + overFifty);
+    
+    DecimalFormatSymbols formatSeparator = new DecimalFormatSymbols(Locale.US);
+    formatSeparator.setDecimalSeparator('.');
+    DecimalFormat decimal = new DecimalFormat("0.0", formatSeparator);
+    
+    if (allCustomers.size() == 0) {
+      System.out.println("----- Percentual -----");
+      System.out.println("menores: 0%");
+      System.out.println("adultas: 0%");
+      System.out.println("a partir de 50: 0%");
+      System.out.println("TOTAL: 0");
+    }
+    
+    if (allCustomers.size() != 0) {
+      float allCustomersFloat = (float) allCustomers.size();
+      float underAgePercentage = (underAges / allCustomersFloat) * 100;
+      float adultsPercentage = (adults / allCustomersFloat) * 100;
+      float overFiftyPercentage = (overFifty / allCustomersFloat) * 100;
+
+      System.out.println("----- Percentual -----");
+      System.out.println("menores: " + decimal.format(underAgePercentage).toString() + "%");
+      System.out.println("adultas: " + decimal.format(adultsPercentage).toString() + "%");
+      System.out.println("a partir de 50: " + decimal.format(overFiftyPercentage).toString() + "%");
+      System.out.println("TOTAL: " + allCustomers.size());
+    }
   }
 }
